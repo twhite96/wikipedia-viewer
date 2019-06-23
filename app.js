d06601dac7907a411ed61eaecf3b98c4acb5e1c7
Tiffany White <hey@tiffanyrwhite.com>
2019-06-23 13:47:28 -0400

    Add comments for further refactoring

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 function getArticles() {
+  // Can't use let here and even var won't work; its scoped to this block
+  // May need query to be global for now
   let query = '';
   let url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=' + `${query}` + '&gsrlimit=5&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max';
   const input = document.querySelector('input');
   const search = document.getElementById('search');
 
-  async function instantSearch (event) {
+  // Function is alllll kinds of wrong here
+  // Why async await here?
+  async function instantSearch(event) {
+    // This is wrong
     query = search.value;
     search.textContent += `${event.code}`;
     console.log(json);
+    // You're fetching a URL that is assigned to and scoped to
+    // another function Fix this!
     return await fetch(url).then(response => response.json());
   }
   input.addEventListener('keyup', instantSearch);
