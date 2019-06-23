89a6b5013ea46f10270c99bca623d95c964cef7c
Tiffany White <hey@tiffanyrwhite.com>
2019-06-23 13:35:43 -0400

    New branch for refactor

--- app.js
+++ app.js
@@ -1,59 +1,23 @@
-// function getArticles() {
-//   var query = "";
-//   var content = [];
-//   query = document.getElementById("search").value;
-// fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=" +
-//     query + "&gsrlimit=5&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max")
-//     .then(function(resp) {
-//       console.log(resp);
-//       return resp.json();
-//     })
-//     .then(function(data) {
-//       console.log(data);
-//     });
-//   var ul = document.createElement("ul");
-//   document.querySelector("articles").appendChild(ul);
-//   content.forEach(renderArticles);
+function getArticles() {
+  let query = '';
+  let url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=' + `${query}` + '&gsrlimit=5&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max';
+  const input = document.querySelector('input');
+  const search = document.getElementById('search');
 
-//   function renderArticles(element, index, arr) {
-//     var li = document.createElement("li");
-//     li.setAttribute("class", "item");
-//     ul.appendChild(li);
-//     li.innerHTML = li.innerHTML + element;
+  async function instantSearch (event) {
+    query = search.value;
+    search.textContent += `${event.code}`;
+    console.log(json);
+    return await fetch(url).then(response => response.json());
+  }
+  input.addEventListener('keyup', instantSearch);
+}
 
-//     var keyWasPressed = document.querySelectorAll(".icon");
+const clearInput = () => {
+  document.getElementById('search').value = null;
+}
 
-//     keyWasPressed[0].addEventListener("keyup", function() {
-//       getArticles();
-//     });
-//   }
-
-$(document).ready(function () {
-  var query = "";
-  var url = "";
-
-  $("#search").keyup(function () {
-    query = $("#search").val();
-    url =
-      "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
-      query +
-      "&format=json&callback=?";
-    $.getJSON(url, function (data) {
-      $("#articles").html("");
-      for (i = 0; i < data[1].length; i++) {
-        $("#articles").append(
-          "<div id=content><a href=" +
-          data[3][i] +
-          " ><h2>" +
-          data[1][i] +
-          "</h2><p>" +
-          data[2][i] +
-          "</p></a></div>"
-        );
-      }
-    });
-  });
-});
+getArticles();
 
 function pullRandoArticle() {
   var getRando = document.getElementById("rando");
@@ -61,5 +25,4 @@ function pullRandoArticle() {
     window.open("https://en.wikipedia.org/wiki/Special:Random");
   });
 }
-
 pullRandoArticle();
